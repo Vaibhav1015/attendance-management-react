ee9ac9d695d5d73c6acc550863358f154129831e
dasharath-491 <khichadedasharath@gmail.com>
2023-06-23 12:56:15 +0530

    css

--- src/app/views/Attendance/AddAttendance.js
+++ src/app/views/Attendance/AddAttendance.js
@@ -1,113 +1,113 @@
-// import React from "react";
-// import { useState } from "react";
-// import { useForm } from "react-hook-form";
+import React from "react";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 
-// const AddAttendance = ({ id, arialabelledby, tabIndex, className, ariaHidden, }) => {
-//     const { register, handleSubmit, control, reset, formState: { errors }, } = useForm();
-//     const [showForm, setShowForm] = useState(true);
-//     const onSubmit = () => { };
-//     return (
-//         <div>
-//             <div
-//                 className={showForm ? className : "modal fade d-none"}
-//                 data-bs-backdrop="false"
-//                 id={id}
-//                 tabIndex={tabIndex}
-//                 aria-labelledby={arialabelledby}
-//                 aria-hidden={showForm ? ariaHidden : "false"}
-//             >
-//                 <div className="modal-dialog modal-lg modal-dialog-centered ">
-//                     <div className="modal-content ">
-//                         <div className="modal-header">
-//                             <h1 className="modal-title fs-5" id="exampleModalLabel">
-//                                 Add Attendance
-//                             </h1>
-//                             <button
-//                                 type="button"
-//                                 className="btn-close"
-//                                 aria-label="Close"
-//                                 data-bs-dismiss="modal"
-//                             ></button>
-//                         </div>
-//                         <div className="addAttendance-form modal-body">
-//                             <form onSubmit={handleSubmit(onSubmit)}>
-//                                 <div className="addAttendance_main-form ">
-//                                     <div className="attendance_date">
-//                                         <label className="date_label">Attendance Date</label>
-//                                         <input
-//                                             type="date"
-//                                             className="dates mb-3"
-//                                             {...register("date", {
-//                                                 required: "Date is required",
-//                                                 valueAsDate: true,
-//                                             })}
-//                                         />
-//                                         {errors.date && (
-//                                             <span className=" text-danger">
-//                                                 Start date is required
-//                                             </span>
-//                                         )}
-//                                     </div>
-//                                     <div>
-//                                         <table className="table table-bordered">
-//                                             <thead className="thead text-center">
-//                                                 <tr>
-//                                                     <th scope="col">Roll No</th>
-//                                                     <th scope="col">Student Name</th>
-//                                                     <th scope="col">Present</th>
-//                                                     <th scope="col">Absent</th>
-//                                                 </tr>
-//                                             </thead>
-//                                             <tbody>
-//                                                 <tr className="text-center">
-//                                                     <td>1</td>
-//                                                     <td>Vaibhav Andhale</td>
-//                                                     <td><input type="radio" name="row1" value="option1" /></td>
-//                                                     <td><input type="radio" name="row1" value="option2" /></td>
+const AddAttendance = ({ id, arialabelledby, tabIndex, className, ariaHidden, }) => {
+    const { register, handleSubmit, control, reset, formState: { errors }, } = useForm();
+    const [showForm, setShowForm] = useState(true);
+    const onSubmit = () => { };
+    return (
+        <div>
+            <div
+                className={showForm ? className : "modal fade d-none"}
+                data-bs-backdrop="false"
+                id={id}
+                tabIndex={tabIndex}
+                aria-labelledby={arialabelledby}
+                aria-hidden={showForm ? ariaHidden : "false"}
+            >
+                <div className="modal-dialog modal-lg modal-dialog-centered ">
+                    <div className="modal-content ">
+                        <div className="modal-header">
+                            <h1 className="modal-title fs-5" id="exampleModalLabel">
+                                Add Attendance
+                            </h1>
+                            <button
+                                type="button"
+                                className="btn-close"
+                                aria-label="Close"
+                                data-bs-dismiss="modal"
+                            ></button>
+                        </div>
+                        <div className="addAttendance-form modal-body">
+                            <form onSubmit={handleSubmit(onSubmit)}>
+                                <div className="addAttendance_main-form ">
+                                    <div className="attendance_date">
+                                        <label className="date_label">Attendance Date</label>
+                                        <input
+                                            type="date"
+                                            className="dates mb-3"
+                                            {...register("date", {
+                                                required: "Date is required",
+                                                valueAsDate: true,
+                                            })}
+                                        />
+                                        {errors.date && (
+                                            <span className=" text-danger">
+                                                Start date is required
+                                            </span>
+                                        )}
+                                    </div>
+                                    <div>
+                                        <table className="table table-bordered">
+                                            <thead className="thead text-center">
+                                                <tr>
+                                                    <th scope="col">Roll No</th>
+                                                    <th scope="col">Student Name</th>
+                                                    <th scope="col">Present</th>
+                                                    <th scope="col">Absent</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <tr className="text-center">
+                                                    <td>1</td>
+                                                    <td>Vaibhav Andhale</td>
+                                                    <td><input type="radio" name="row1" value="option1" /></td>
+                                                    <td><input type="radio" name="row1" value="option2" /></td>
 
-//                                                 </tr>
-//                                                 <tr className="text-center">
-//                                                     <td>2</td>
-//                                                     <td>Vaibhav Andhale</td>
-//                                                     <td><input type="radio" name="row2" value="option3" /></td>
-//                                                     <td><input type="radio" name="row2" value="option2" /></td>
-//                                                 </tr>
-//                                                 <tr className="text-center">
-//                                                     <td>3</td>
-//                                                     <td>Vaibhav Andhale</td>
-//                                                     <td><input type="radio" name="row1" value="option1" /></td>
-//                                                     <td><input type="radio" name="row1" value="option2" /></td>
+                                                </tr>
+                                                <tr className="text-center">
+                                                    <td>2</td>
+                                                    <td>Vaibhav Andhale</td>
+                                                    <td><input type="radio" name="row2" value="option3" /></td>
+                                                    <td><input type="radio" name="row2" value="option2" /></td>
+                                                </tr>
+                                                <tr className="text-center">
+                                                    <td>3</td>
+                                                    <td>Vaibhav Andhale</td>
+                                                    <td><input type="radio" name="row1" value="option1" /></td>
+                                                    <td><input type="radio" name="row1" value="option2" /></td>
 
-//                                                 </tr>
-//                                                 <tr className="text-center">
-//                                                     <td>4</td>
-//                                                     <td>Vaibhav Andhale</td>
-//                                                     <td><input type="radio" name="row2" value="option3" /></td>
-//                                                     <td><input type="radio" name="row2" value="option2" /></td>
-//                                                 </tr>
-//                                             </tbody>
-//                                         </table>
-//                                     </div>
-//                                 </div>
-//                                 <div className="attendance-footer">
-//                                     <button className="btn btn-success" type="submit">
-//                                         Add
-//                                     </button>
-//                                     <button
-//                                         type="button"
-//                                         className="btn btn-danger"
-//                                         data-bs-dismiss="modal"
-//                                         onClick={() => reset()}
-//                                     >
-//                                         Close
-//                                     </button>
-//                                 </div>
-//                             </form>
-//                         </div>
-//                     </div>
-//                 </div>
-//             </div >
-//         </div >
-//     );
-// };
-// export default AddAttendance;
+                                                </tr>
+                                                <tr className="text-center">
+                                                    <td>4</td>
+                                                    <td>Vaibhav Andhale</td>
+                                                    <td><input type="radio" name="row2" value="option3" /></td>
+                                                    <td><input type="radio" name="row2" value="option2" /></td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                                <div className="attendance-footer">
+                                    <button className="btn btn-success" type="submit">
+                                        Add
+                                    </button>
+                                    <button
+                                        type="button"
+                                        className="btn btn-danger"
+                                        data-bs-dismiss="modal"
+                                        onClick={() => reset()}
+                                    >
+                                        Close
+                                    </button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div >
+        </div >
+    );
+};
+export default AddAttendance;
